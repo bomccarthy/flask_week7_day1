70c360c600bd44d13186aee445537793310c7864
bomccarthy <110850293+bomccarthy@users.noreply.github.com>
2022-11-03 17:59:55 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+// function multiply(a, b){
+//     return a * b
+//   }
+// 
+// console.log(multiply(2, 4))
+//------------------------------------------------------------------------------
+// function evenOrOdd(number) {
+//   if (number % 2 === 0){
+//     return 'Even'
+//   } else {
+//     return 'Odd'
+//   }
+// }
+// 
+// console.log(evenOrOdd(2))
+// console.log(evenOrOdd(3))
+// console.log(evenOrOdd(1023795))
+//------------------------------------------------------------------------------
